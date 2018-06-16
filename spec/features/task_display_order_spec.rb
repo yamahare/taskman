8fd0944986235e8d96a40dda93c086db20cd062f
yamahare <jidai.norio@gmail.com>
2018-06-16 17:26:22 +0900

    [add]優先度によるソートのテスト追加

--- spec/features/task_display_order_spec.rb
+++ spec/features/task_display_order_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature 'タスクの表示順', type: :feature do
   describe 'indexページ' do
     let(:valid_attrs) do
-      [{ name: '最初につくったタスク',created_at: DateTime.now,  end_date: Date.today},
-       { name: '2番めにつくったタスク',created_at: DateTime.now + 1, end_date: Date.today.next_year },
-       { name: '3番めにつくったタスク',created_at: DateTime.now + 2, end_date: Date.today.tomorrow }]
+      [{ name: '最初につくったタスク', created_at: DateTime.now,     end_date: Date.today,           priority: 3},
+       { name: '2番めにつくったタスク',created_at: DateTime.now + 1, end_date: Date.today.next_year, priority: 1},
+       { name: '3番めにつくったタスク',created_at: DateTime.now + 2, end_date: Date.today.tomorrow , priority: 2}]
     end
     let(:task_names) { page.all('.task_name') }
     before do
@@ -21,8 +21,10 @@ RSpec.feature 'タスクの表示順', type: :feature do
       end
     end
 
-    context '▼をクリックした場合' do
-      before { click_link('▼') }
+    context '終了日の▼をクリックした場合' do
+      before do
+        page.find('th', text:'終了日').click_link('▼')
+      end
       it '終了日の降順に表示されること' do
         expect(task_names[0].text).to eq valid_attrs[1][:name]
         expect(task_names[1].text).to eq valid_attrs[2][:name]
@@ -30,12 +32,36 @@ RSpec.feature 'タスクの表示順', type: :feature do
       end
     end
 
-    context '▲をクリックした場合' do
-      before { click_link('▲') }
+    context '終了日の▲をクリックした場合' do
+      before do
+        page.find('th', text:'終了日').click_link('▲')
+      end
       it '終了日の昇順に表示されること' do
         expect(task_names[0].text).to eq valid_attrs[0][:name]
         expect(task_names[1].text).to eq valid_attrs[2][:name]
         expect(task_names[2].text).to eq valid_attrs[1][:name]
+      end
+    end
+
+    context '優先度の▼をクリックした場合' do
+      before do
+        page.find('th', text:'優先度').click_link('▼')
+      end
+      it '優先度の降順に表示されること' do
+        expect(task_names[0].text).to eq valid_attrs[0][:name]
+        expect(task_names[1].text).to eq valid_attrs[2][:name]
+        expect(task_names[2].text).to eq valid_attrs[1][:name]
+      end
+    end
+
+    context '優先度の▲をクリックした場合' do
+      before do
+        page.find('th', text:'優先度').click_link('▲')
+      end
+      it '優先度の昇順に表示されること' do
+        expect(task_names[0].text).to eq valid_attrs[1][:name]
+        expect(task_names[1].text).to eq valid_attrs[2][:name]
+        expect(task_names[2].text).to eq valid_attrs[0][:name]
       end
     end
   end
