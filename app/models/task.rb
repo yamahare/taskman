516889156080e27fafe48309466c2eaba7134b25
yamahare <jidai.norio@gmail.com>
2018-08-08 00:09:49 +0900

    [add]labelモデル追加。taskとlabelのリレーションモデル追加。

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,6 +10,10 @@ class Task < ApplicationRecord
 
   belongs_to :user
 
+  has_many :task_labels, dependent: :destroy
+  has_many :labels, through: :task_labels
+  accepts_nested_attributes_for :task_labels, allow_destroy: true
+
   scope :like_username, ->(name) do
     if name.present?
       where("name LIKE ?", "%#{name}%")
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_113519) do
+ActiveRecord::Schema.define(version: 2018_08_07_143619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "task_labels", force: :cascade do |t|
+    t.bigint "task_id"
+    t.bigint "label_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label_id"], name: "index_task_labels_on_label_id"
+    t.index ["task_id"], name: "index_task_labels_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.text "name", null: false
@@ -40,5 +55,7 @@ ActiveRecord::Schema.define(version: 2018_08_05_113519) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "task_labels", "labels"
+  add_foreign_key "task_labels", "tasks"
   add_foreign_key "tasks", "users"
 end
